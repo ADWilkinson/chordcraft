17178e80abd79c1e72413e40279e25913707f5a1
Davy Jones <andywilkinson1993@gmail.com>
2023-05-16 12:13:28 +0100

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/tailwind.css'
 import 'focus-visible'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
-        <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   )
 }
